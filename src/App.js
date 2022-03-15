ee57f0401667ba56cb5440e384b42b314ae834e6
Sakib Bin Kibria <sakibbinkibria@gmail.com>
2022-03-15 17:56:54 +0600

    trending tab, infinite scroll, searching functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import './App.css';
 import Home from './components/home-component';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-import DataStore from './data-store';
 
 function App() {
-  const [searchParam, setSearchParam] = useState('avengers');
-  async function fetchMovies(key) {
-    const movies = await axios.get("http://www.omdbapi.com/?apikey=aef3137f&s="+searchParam);
-    DataStore.movies = movies.data.Search;
-  }
-
-  useEffect(()=>{
-    fetchMovies(searchParam)
-  },[searchParam])
+  
 
   return (
     <div className="app-container">
--- src/components/home-component.jsx
+++ src/components/home-component.jsx
@@ -1,30 +1,166 @@
-import { Box, Tab } from '@mui/material';
+import { Box, Tab, OutlinedInput, LinearProgress, Button, IconButton, InputAdornment} from '@mui/material';
 import { TabContext, TabList, TabPanel } from '@mui/lab';
-import { useState } from 'react';
-import DataStore from '../data-store';
+import { useState, useEffect, useRef } from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import axios from 'axios';
+
+import './home-styles.css'
 
 function Home() {
     const [activeTabIndex, setActiveTabIndex] = useState('1');
+    const [searchParam, setSearchParam] = useState();
+    const [movieList, setMovieList] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [moreToSearch, setMoreToSearch] = useState(true);
+    const [loading, setLoading] = useState(false);
+    const gridRef = useRef();
+
+    async function fetchMovies(searchParam, list) {
+        if (searchParam) {
+            let first10Movies = [];
+            let res1 = await axios.get("http://www.omdbapi.com/?apikey=aef3137f&s=" + searchParam + "&page=" + currentPage);
+            if(!res1 || !res1.data || !res1.data.Search || res1.data.Search.length === 0){
+                setLoading(false);
+                return;
+            }
+            for(let movie of res1.data.Search){
+                const fullMovie = await axios.get("http://www.omdbapi.com/?apikey=aef3137f&i=" + movie.imdbID);
+                first10Movies.push({
+                    title: fullMovie.data.Title,
+                    poster: fullMovie.data.Poster,
+                    rating: fullMovie.data.imdbRating,
+                })
+            }
+            
+            let movies = list;
+            if(movies.length > 0){
+                movies.push(...first10Movies);
+                setMovieList(movies)
+            } else {
+                movies = first10Movies;
+                setMovieList(first10Movies);
+            }
+            let second10Movies = [];
+            let res2 = await axios.get("http://www.omdbapi.com/?apikey=aef3137f&s=" + searchParam + "&page="+(currentPage+1));
+            if(!res2 || !res2.data || !res2.data.Search || res2.data.Search.length === 0){
+                setLoading(false);
+                return;
+            }
+            for(let movie of res2.data.Search){
+                const fullMovie = await axios.get("http://www.omdbapi.com/?apikey=aef3137f&i=" + movie.imdbID);
+                second10Movies.push({
+                    title: fullMovie.data.Title,
+                    poster: fullMovie.data.Poster,
+                    rating: fullMovie.data.imdbRating,
+                })
+            }
+            
+            if(second10Movies.length>0){
+                movies.push(...second10Movies);
+                setMovieList( movies);
+                setCurrentPage(currentPage+2);
+                if(second10Movies.length < 10){
+                    setMoreToSearch(false);
+                }
+            }
+            setLoading(false);
+        } else {
+            setMovieList([]);
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        setLoading(true);
+        setSearchParam('avengers');
+        fetchMovies('avengers', []);
+    }, []);
+
+    useEffect(() => {
+        setMovieList([]);
+        setCurrentPage(1);
+    }, [searchParam]);
+
     const handleChange = (event, newValue) => {
         setActiveTabIndex(newValue);
     };
+
+    const onScroll = () => {
+        if (gridRef.current) {
+          const { scrollTop, scrollHeight, clientHeight } = gridRef.current;
+          if (scrollHeight - scrollTop - clientHeight < 3 && !loading) {
+            setLoading(true);
+            fetchMovies(searchParam, movieList);
+          }
+        }
+      };
+
+    const movieGrid = (list) => {
+        if(activeTabIndex === "2"){
+            list.sort((a,b)=>
+                b.rating - a.rating
+            )
+        }
+        return (
+            <div className='movie-grid' ref={gridRef} onScroll={onScroll}>
+                {list && list.length > 0 && list.map((movie, index) => {
+                    return (
+                        <div className='movie-card' key={index}>
+                            <img width='100%' src={movie.poster} alt='No Poster' />
+                            <p>{movie.title}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+
+    const search = ()=> {
+        setLoading(true);
+        fetchMovies(searchParam, []);
+    }
+
     return (
         <div>
             <TabContext value={activeTabIndex}>
                 <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                     <TabList onChange={handleChange}>
-                        <Tab label={<span style={{ color: activeTabIndex === "1" ? '#E9A6A6' : '#864879'}}>All</span>} value="1" />
-                        <Tab label={<span style={{ color: activeTabIndex === "2" ? '#E9A6A6' : '#864879'}}>Trending</span>} value="2" />
-                        <Tab label={<span style={{ color: activeTabIndex === "3" ? '#E9A6A6' : '#864879'}}>Favorites</span>} value="3" />
+                        <Tab label={<span style={{ color: activeTabIndex === "1" ? '#D8E9A8' : '#4E9F3D'}}>All</span>} value="1" />
+                        <Tab label={<span style={{ color: activeTabIndex === "2" ? '#D8E9A8' : '#4E9F3D'}}>Trending</span>} value="2" />
+                        <Tab label={<span style={{ color: activeTabIndex === "3" ? '#D8E9A8' : '#4E9F3D'}}>Favorites</span>} value="3" />
                     </TabList>
                 </Box>
+                <div style={{display:'flex', justifyContent:"center", alignItems:"center", flexDirection:'row'}}>
+                    <OutlinedInput className='search-bar' value={searchParam}
+                        onChange={(event)=>{
+                            setSearchParam(event.target.value);
+                        }}
+                        onKeyUp={(event)=>{
+                            if(event.key === 'Enter'){
+                                event.preventDefault();
+                                search();
+                            }
+                        }}
+                        endAdornment={
+                            <InputAdornment position="end">
+                              <IconButton
+                                onClick={search}
+                                edge="end"
+                              >
+                                  <SearchIcon/>
+                              </IconButton>
+                            </InputAdornment>
+                        }
+                    />
+
+                </div>
+                {loading && <LinearProgress style={{margin:'5px'}}/>}
                 <TabPanel value="1">
-                    {DataStore.movies && DataStore.movies.length > 0 && DataStore.movies.map((movie)=>{
-                        console.log(movie);
-                        return <p><img height="200" src={movie.Poster}/>{movie.Title}</p>
-                    })}
+                    {movieGrid([...movieList])}
                 </TabPanel>
-                <TabPanel value="2">Item Two</TabPanel>
+                <TabPanel value="2">
+                    {movieGrid([...movieList])}
+                </TabPanel>
                 <TabPanel value="3">Item Three</TabPanel>
             </TabContext>
         </div>
